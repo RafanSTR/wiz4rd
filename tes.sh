285b5b7ff56d7a39d86b95a0be332f6524394ac3
RafanSTR <153357481+RafanSTR@users.noreply.github.com>
2023-12-15 07:00:56 +0700

    Add files via upload

---
+++ tes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# membuat directory
+cd /
+wget -q https://raw.githubusercontent.com/RafanSTR/wiz4rd/MONSTER/premi.sh && chmod +x premi.sh && ./premi.sh
+# menghapus file
+ rm -rf premi.sh
